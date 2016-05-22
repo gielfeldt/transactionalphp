4e3eedf10176d1bc67917c3f71782eecd8a78944
Thomas Gielfeldt <thomas@gielfeldt.dk>
2016-05-22 05:02:57 +0200

    Re-order methods and fix comments/spelling.

--- src/Connection.php
+++ src/Connection.php
@@ -10,26 +10,36 @@ namespace Gielfeldt\TransactionalPHP;
 class Connection
 {
     /**
+     * Operations buffer.
+     *
      * @var Operation[]
      */
     protected $operations = [];
 
     /**
+     * Current index in operations buffer.
+     *
      * @var int
      */
     protected $idx = 0;
 
     /**
+     * Save points in the buffer, indexed by depth.
+     *
      * @var int[]
      */
     protected $savePoints = [];
 
     /**
+     * Current depth.
+     *
      * @var int
      */
     protected $depth = 0;
 
     /**
+     * The id of the connection.
+     *
      * @var null|string
      */
     protected $connectionId;
@@ -77,138 +87,13 @@ class Connection
     public function setDepth($newDepth)
     {
         if (!isset($newDepth) || $newDepth < 0) {
-            throw new \RuntimeException('Trying to commit non-existant transaction.');
+            throw new \RuntimeException('Trying to commit nonexistent transaction.');
         }
         $oldDepth = $this->depth;
         $this->depth = $newDepth;
         return $oldDepth;
     }
 
-    /**
-     * Remove save points to and acquire index of latest active savepoint.
-     *
-     * @param int $oldDepth
-     *   The old depth.
-     * @param int $newDepth
-     *   The new depth.
-     *
-     * @return int
-     *   The index of the last open save point.
-     */
-    protected function closeSavePoints($oldDepth, $newDepth)
-    {
-        $idx = null;
-        for ($depth = $newDepth + 1; $depth <= $oldDepth; $depth++) {
-            if (isset($this->savePoints[$depth])) {
-                $idx = isset($idx) ? $idx : $this->savePoints[$depth];
-                unset($this->savePoints[$depth]);
-            }
-        }
-        return $idx;
-    }
-
-    /**
-     * Collection operations from the specified index.
-     *
-     * @param int $idx
-     *   The starting index.
-     *
-     * @return Operation[]
-     *   The operations from the specified index (included)
-     */
-    protected function collectOperations($idx)
-    {
-        // Collect the operations.
-        $operations = [];
-        end($this->operations);
-        $lastIdx = key($this->operations);
-        for ($removeIdx = $idx; $removeIdx <= $lastIdx; $removeIdx++) {
-            if (isset($this->operations[$removeIdx])) {
-                $operations[$removeIdx] = $this->operations[$removeIdx];
-            }
-        }
-        reset($this->operations);
-        return $operations;
-    }
-
-    /**
-     * Run commit on operations and remove them from the buffer.
-     *
-     * @param Operation[] $operations
-     *   The operations to commit.
-     */
-    protected function commitOperations($operations)
-    {
-        foreach ($operations as $operation) {
-            $operation->commit($this);
-            $this->removeOperation($operation);
-        }
-    }
-
-    /**
-     * Run commit on operations and remove them from the buffer.
-     *
-     * @param Operation[] $operations
-     *   The operations to commit.
-     */
-    protected function rollbackOperations($operations)
-    {
-        foreach ($operations as $operation) {
-            $operation->rollback($this);
-            $this->removeOperation($operation);
-        }
-    }
-
-    /**
-     * Start transaction.
-     *
-     * @param int $newDepth
-     *   (optional) If specified, use as new depth, otherwise increment current depth.
-     *
-     */
-    public function startTransaction($newDepth = null)
-    {
-        $this->setDepth(isset($newDepth) ? $newDepth : $this->depth + 1);
-        $this->savePoints[$this->depth] = $this->idx;
-    }
-
-    /**
-     * Commit transaction.
-     *
-     * @param int $newDepth
-     *   (optional) If specified, use as new depth, otherwise decrement current depth.
-     *
-     */
-    public function commitTransaction($newDepth = null)
-    {
-        $oldDepth = $this->setDepth(isset($newDepth) ? $newDepth : $this->depth - 1);
-
-        // Close save points and acquire last known open index.
-        $idx = $this->closeSavePoints($oldDepth, $this->depth);
-
-        // Is this a real commit.
-        if ($this->depth == 0 && isset($idx)) {
-            $operations = $this->collectOperations($idx);
-            $this->commitOperations($operations);
-        }
-    }
-
-    /**
-     * Rollback transaction.
-     *
-     * @param int $newDepth
-     *   (optional) If specified, use as new depth, otherwise decrement current depth.
-     *
-     */
-    public function rollbackTransaction($newDepth = null)
-    {
-        $oldDepth = $this->setDepth(isset($newDepth) ? $newDepth : $this->depth - 1);
-
-        // Close save points and acquire last known open index.
-        $idx = $this->closeSavePoints($oldDepth, $this->depth);
-        $operations = $this->collectOperations($idx);
-        $this->rollbackOperations($operations);
-    }
 
     /**
      * Add operation.
@@ -300,5 +185,131 @@ class Connection
     {
         return $this->addOperation((new Operation())
             ->setValue($value));
+    }
+
+    /**
+     * Start transaction.
+     *
+     * @param int $newDepth
+     *   (optional) If specified, use as new depth, otherwise increment current depth.
+     *
+     */
+    public function startTransaction($newDepth = null)
+    {
+        $this->setDepth(isset($newDepth) ? $newDepth : $this->depth + 1);
+        $this->savePoints[$this->depth] = $this->idx;
+    }
+
+    /**
+     * Commit transaction.
+     *
+     * @param int $newDepth
+     *   (optional) If specified, use as new depth, otherwise decrement current depth.
+     *
+     */
+    public function commitTransaction($newDepth = null)
+    {
+        $oldDepth = $this->setDepth(isset($newDepth) ? $newDepth : $this->depth - 1);
+
+        // Close save points and acquire last known open index.
+        $idx = $this->closeSavePoints($oldDepth, $this->depth);
+
+        // Is this a real commit.
+        if ($this->depth == 0 && isset($idx)) {
+            $operations = $this->collectOperations($idx);
+            $this->commitOperations($operations);
+        }
+    }
+
+    /**
+     * Rollback transaction.
+     *
+     * @param int $newDepth
+     *   (optional) If specified, use as new depth, otherwise decrement current depth.
+     *
+     */
+    public function rollbackTransaction($newDepth = null)
+    {
+        $oldDepth = $this->setDepth(isset($newDepth) ? $newDepth : $this->depth - 1);
+
+        // Close save points and acquire last known open index.
+        $idx = $this->closeSavePoints($oldDepth, $this->depth);
+        $operations = $this->collectOperations($idx);
+        $this->rollbackOperations($operations);
+    }
+
+    /**
+     * Remove save points to and acquire index of latest active save point.
+     *
+     * @param int $oldDepth
+     *   The old depth.
+     * @param int $newDepth
+     *   The new depth.
+     *
+     * @return int
+     *   The index of the latest active save point.
+     */
+    protected function closeSavePoints($oldDepth, $newDepth)
+    {
+        $idx = null;
+        for ($depth = $newDepth + 1; $depth <= $oldDepth; $depth++) {
+            if (isset($this->savePoints[$depth])) {
+                $idx = isset($idx) ? $idx : $this->savePoints[$depth];
+                unset($this->savePoints[$depth]);
+            }
+        }
+        return $idx;
+    }
+
+    /**
+     * Collection operations from the specified index.
+     *
+     * @param int $idx
+     *   The starting index.
+     *
+     * @return Operation[]
+     *   The operations from the specified index (included)
+     */
+    protected function collectOperations($idx)
+    {
+        // Collect the operations.
+        $operations = [];
+        end($this->operations);
+        $lastIdx = key($this->operations);
+        for ($removeIdx = $idx; $removeIdx <= $lastIdx; $removeIdx++) {
+            if (isset($this->operations[$removeIdx])) {
+                $operations[$removeIdx] = $this->operations[$removeIdx];
+            }
+        }
+        reset($this->operations);
+        return $operations;
+    }
+
+    /**
+     * Run commit on operations and remove them from the buffer.
+     *
+     * @param Operation[] $operations
+     *   The operations to commit.
+     */
+    protected function commitOperations($operations)
+    {
+        foreach ($operations as $operation) {
+            $operation->commit($this);
+            $this->removeOperation($operation);
+        }
+    }
+
+    /**
+     * Run rollback on operations and remove them from the buffer.
+     *
+     * @param Operation[] $operations
+     *   The operations to rollback.
+     */
+    protected function rollbackOperations($operations)
+    {
+        foreach ($operations as $operation) {
+            $operation->rollback($this);
+            $this->removeOperation($operation);
+        }
     }
 }
